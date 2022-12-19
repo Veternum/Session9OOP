9f795744801dfdff9ecc2d7e66dc761e833fd412
Veternum <40434034+Veternum@users.noreply.github.com>
2022-12-20 04:53:28 +0700

    Add files via upload

--- GachaSimulator/src/Main.java
+++ GachaSimulator/src/Main.java
@@ -16,38 +16,7 @@ public class Main {
 
 	// Tujuan Program adalah untuk membuat simulasi Gacha
 	public Main() {
-		// Sampel Input:
-		// Pecorine 1 Physical
-		// Kyaru 1 Magical
-		// Kokkoro 1 Physical
-		// Yukari 1 Physical
-		// Yori 1 Magical
-		// Yui 1 Magical
-		// Misato 2 Magical
-		// Miyako 2 Physical
-		// Eriko 2 Physical
-		// Akari 2 Magical
-		// Muimi 3 Physical
-		// Neneka 3 Magical
-//		characterList.add(new Physical("Pecorine", 1, "Physical"));
-//		characterList.add(new Magical("Kyaru", 1, "Magical"));
-//		characterList.add(new Physical("Kokkoro", 1, "Physical"));
-//		characterList.add(new Physical("Yukari", 1, "Physical"));
-//		characterList.add(new Magical("Yori", 1, "Magical"));
-//		characterList.add(new Magical("Yui", 1, "Magical"));
-//		characterList.add(new Magical("Misato", 2, "Magical"));
-//		characterList.add(new Physical("Miyako", 2, "Physical"));
-//		characterList.add(new Physical("Eriko", 2, "Physical"));
-//		characterList.add(new Magical("Akari", 2, "Magical"));
-//		characterList.add(new Physical("Muimi", 3, "Physical"));
-//		characterList.add(new Magical("Neneka", 3, "Magical"));
-
-		// Rate 1*: 77
-//		a = 77;
-		// Rate 2*: 18
-//		b =  18;
-		// Rate 3*: 5
-//		c = 5;
+		addDummy(); // Default Data
 		int choice = 0;
 		do {
 			System.out.println("Menu");
@@ -234,6 +203,21 @@ public class Main {
 		}
 		System.out.println("Press Enter to Continue...");
 		input.nextLine();
+	}
+	
+	private void addDummy() {
+		characterList.add(new Physical("Pecorine", 1, "Physical"));
+		characterList.add(new Magical("Kyaru", 1, "Magical"));
+		characterList.add(new Physical("Kokkoro", 1, "Physical"));
+		characterList.add(new Physical("Yukari", 1, "Physical"));
+		characterList.add(new Magical("Yori", 1, "Magical"));
+		characterList.add(new Magical("Yui", 1, "Magical"));
+		characterList.add(new Magical("Misato", 2, "Magical"));
+		characterList.add(new Physical("Miyako", 2, "Physical"));
+		characterList.add(new Physical("Eriko", 2, "Physical"));
+		characterList.add(new Magical("Akari", 2, "Magical"));
+		characterList.add(new Physical("Muimi", 3, "Physical"));
+		characterList.add(new Magical("Neneka", 3, "Magical"));
 	}
 
 	public static void main(String[] args) {
